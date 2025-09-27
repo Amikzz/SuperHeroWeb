c6df2485376dab23029c73d735aa4c7cd83a0d7b
Amika Subasinghe <amixx2005@gmail.com>
2025-09-27 17:49:07 +0530

    about connet added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Heroes } from './components/heroes/heroes';
 import { Search } from './components/search/search';
+import { About } from './components/about/about';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'heroes', component: Heroes },
-  { path: 'search', component: Search}
+  { path: 'search', component: Search},
+  { path: 'about', component: About}
 ];
 
